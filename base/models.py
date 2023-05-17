754768ed03d4492068804dd6204f444a464272bf
mohammad <mohammadjamshidi364@gmail.com>
2023-05-17 19:48:54 +0330

    fix

--- base/models.py
+++ base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    avatar = models.ImageField(null=True , blank=True , default="/static/media/avatar.svg", upload_to="")
+    avatar = models.ImageField(null=True , blank=True , default="/avatar.svg")
     bio = models.TextField(null=True , blank=True)
     
     def __str__(self):
